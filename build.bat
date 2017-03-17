973686b14fb7faf129797b0c906cef099cc66097
Hao <a471558277@gmail.com>
2017-03-17 10:44:43 +0800

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cd /d %1
-set compilerflags=/Od /Zi /EHsc /Wall /std:c++14
-set linkerflags=/Fe:.build/main.exe
-cl.exe %compilerflags% %2 %linkerflags%
-del %3.obj

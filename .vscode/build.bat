44b2b04fd188fc178bd4325466620983e182a03c
RickyHao <a471558277@gmail.com>
2017-05-16 11:38:23 +0800

    Upload .vscode

---
+++ .vscode/build.bat
@@ -0,0 +1,7 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd /d %1
+set compilerflags=/Od /Zi /EHsc  /std:c++14
+set linkerflags=/Fe:.build/main.exe
+cl.exe %compilerflags% %2 %linkerflags%
+del %3.obj
